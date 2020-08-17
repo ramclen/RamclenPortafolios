48b9266e249334cef72f8cf0d1bc57f5947d5641
Cristopher <cristopher.correo@gmail.com>
2020-08-17 21:13:51 +0100

    added anime searcher project

--- src/projects.js
+++ src/projects.js
@@ -1,67 +1,79 @@
-const codeBaseURL = "https://github.com/ramclen"
-const demoBaseURL = "https://ramclen.best"
+const codeBaseURL = 'https://github.com/ramclen';
+const demoBaseURL = 'https://ramclen.best';
 export const projects = [
   {
-    name: "Water Tracker App",
-    img: "./img/water-tracker-app.png",
-    description: "Water Tracker helps you to record your water consumptions in the day. Developed using React and AWS",
+    name: 'Water Tracker App',
+    img: './img/water-tracker-app.png',
+    description:
+      'Water Tracker helps you to record your water consumptions in the day. Developed using React and AWS',
     codeLink: `${codeBaseURL}/water-tracker-app`,
-    demoLink: `${demoBaseURL}/water-tracker-app`
+    demoLink: `${demoBaseURL}/water-tracker-app`,
   },
   {
-    name: "Supermarket Kata",
-    img: "./img/supermarket-kata.png",
-    description: "Supermarket Kata is an small implementation of supermaket kata using React following BDD",
+    name: 'Supermarket Kata',
+    img: './img/supermarket-kata.png',
+    description:
+      'Supermarket Kata is an small implementation of supermaket kata using React following BDD',
     codeLink: `${codeBaseURL}/supermarket-kata`,
     demoLink: `${demoBaseURL}/supermarket-kata`,
   },
   {
-    name: "Streaming Video App",
-    img: "./img/streamblog.png",
-    description: "Streaming Video App that streams your own streaming using a RTMP server, and allows you to create, edit or delete your streams. Developed using React",
-    codeLink: `${codeBaseURL}/video-streamer`
+    name: 'Anime Searcher',
+    img: './img/anime-searcher.png',
+    description:
+      'Using an async autocomplete to search anime shows using Kitsu API. Developed using React',
+    codeLink: `${codeBaseURL}/anime-searcher`,
+    demoLink: `${demoBaseURL}/anime-searcher`,
   },
   {
-    name: "Image searcher",
-    img: "./img/image-searcher.png",
-    description: "Image searcher show you different pictures when user types a hint in the search bar. Developed using React and Unsplash API ",
+    name: 'Streaming Video App',
+    img: './img/streamblog.png',
+    description:
+      'Streaming Video App that streams your own streaming using a RTMP server, and allows you to create, edit or delete your streams. Developed using React',
+    codeLink: `${codeBaseURL}/video-streamer`,
+  },
+  {
+    name: 'Image searcher',
+    img: './img/image-searcher.png',
+    description:
+      'Image searcher show you different pictures when user types a hint in the search bar. Developed using React and Unsplash API ',
     demoLink: `${demoBaseURL}/Image-Searcher`,
-    codeLink: `${codeBaseURL}/Image-Searcher`
+    codeLink: `${codeBaseURL}/Image-Searcher`,
   },
   {
-    name: "Video Visualizer",
-    img: "./img/video-searcher.png",
-    description: "Video searcher using Youtube API. Developed using React ",
+    name: 'Video Visualizer',
+    img: './img/video-searcher.png',
+    description: 'Video searcher using Youtube API. Developed using React ',
 
-    codeLink: `${codeBaseURL}/video-searcher`
+    codeLink: `${codeBaseURL}/video-searcher`,
   },
   {
-    name: "Shopping Cart",
-    img: "./img/shopping-list.png",
-    description: "Shopping list created using react and google spreadsheets API",
+    name: 'Shopping Cart',
+    img: './img/shopping-list.png',
+    description:
+      'Shopping list created using react and google spreadsheets API',
     demoLink: `${demoBaseURL}/shopping-cart`,
-    codeLink: `${codeBaseURL}/ShoppingCart`
+    codeLink: `${codeBaseURL}/ShoppingCart`,
   },
   {
-    name: "Pong game",
-    img: "./img/pong.png",
-    description: "Pong game made using Vanilla JavaScript",
+    name: 'Pong game',
+    img: './img/pong.png',
+    description: 'Pong game made using Vanilla JavaScript',
     demoLink: `${demoBaseURL}/Pong`,
-    codeLink: `${codeBaseURL}/Pong`
+    codeLink: `${codeBaseURL}/Pong`,
   },
   {
-    name: "Snake game",
-    img: "./img/snake-game.png",
-    description: "Snake game made using Vanilla JavaScript",
+    name: 'Snake game',
+    img: './img/snake-game.png',
+    description: 'Snake game made using Vanilla JavaScript',
     demoLink: `${demoBaseURL}/snake`,
-    codeLink: `${codeBaseURL}/snake`
+    codeLink: `${codeBaseURL}/snake`,
   },
   {
-    name: "Tetris game",
-    img: "./img/tetris.png",
-    description: "Tetris game made using Vanilla JavaScript",
+    name: 'Tetris game',
+    img: './img/tetris.png',
+    description: 'Tetris game made using Vanilla JavaScript',
     demoLink: `${demoBaseURL}/Tetris`,
-    codeLink: `${codeBaseURL}/Tetris  `
-  }
-
-]
+    codeLink: `${codeBaseURL}/Tetris  `,
+  },
+];
